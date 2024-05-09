db66ddc91501f034fe03ccee6dd771f571b22e03
gaellebega <gaellebega@gmail.com>
2024-05-09 17:43:05 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+// import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Layout from './Layout/Layout'
-import Admin from './Pages/AdminDash/Admin'
-import Manager from './Pages/ManagerDash/Manager'
-import Teacher from './Pages/TeacherDash/Teacher'
+import Call from './Components/Call'
+// import Layout from './Layout/Layout'
+// import Admin from './Pages/AdminDash/Admin'
+// import Manager from './Pages/ManagerDash/Manager'
+// import Teacher from './Pages/TeacherDash/Teacher'
+// import Home from './Components/Home'
+// import Main from './Components/Main'
+import About from './Components/About'
 
 function App() {
   return (
     <>
     <Router>
       <Routes>
+        <Route path="/" element={<Call/>}/>
+        <Route path="/about" element={<About/>}/>
+        {/* <Route path="/main" element={<Main/>}/>
+       
         <Route path="/" element={<Layout/>}>
           <Route path="/admin" element={<Admin/>}/>
           <Route path="/manager" element={<Manager/>}/>
           <Route path="/teacher" element={<Teacher/>}/>
-          </Route>
+          </Route> */}
       </Routes>
     </Router>
 
