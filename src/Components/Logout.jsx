aa9af5883b51636a79c9e8b86ecef38ef3cd3e48
Teta Iris Credot <tetairiscredot@gmail.com>
2024-05-27 17:59:32 +0200

    Updated rt

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -1,16 +1,25 @@
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+//import { AuthContext } from '../context/AuthContext'; // Assuming you have an AuthContext
 
 function Logout() {
   const navigate = useNavigate();
-
+  const { setAuth } = useContext(AuthContext); // Assuming you have a setAuth method to update authentication status
+  const logoutEndpoints = [
+    'https://umwarimu-loan-hub-api.onrender.com/api/teacher/logout',
+    'https://umwarimu-loan-hub-api.onrender.com/api/manager/logout',
+    'https://umwarimu-loan-hub-api.onrender.com/api/user/logout'
+  ];
   const handleLogout = async (e) => {
     e.preventDefault();
-
+    for (const endpoint of loginEndpoints) {
     try {
-      const response = await axios.post("https://umwarimu-loan-hub-api.onrender.com/api/teacher/logout");
+      const response = await axios.post(  endpoint, {}, { withCredentials: true });
       console.log(response.data); // Logging the response data upon successful logout
       
+      // Clear authentication status
+      setAuth(false);
       navigate('/');
     } catch (error) {
       console.error('Error during logout:', error);
@@ -24,11 +33,11 @@ function Logout() {
         console.error('Error message:', error.message);
       }
     }
-  };
+  }};
 
   return (
     <button onClick={handleLogout} className='p-3 m-2 rounded-md text-md hover:bg-white hover:text-red-500'>
-      Logout
+      Logou
     </button>
   );
 }
