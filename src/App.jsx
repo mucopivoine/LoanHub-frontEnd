c26f1e98036788dbbaa218f372c99f44b4d31e12
Teta Iris Credot <tetairiscredot@gmail.com>
2024-05-30 13:19:09 +0200

    Updated reset

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function App() {
               <Route path="signup" element={<SignUp />} />
               <Route path="forgot" element={<Forgot />} />
               <Route path="forgotps" element={<Forgotps />} />
-              <Route path='reset/:token' element={<Reset />} />
+              <Route path='reset' element={<Reset />} />
               <Route path="otpinput" element={<OtpInput />} />
               <Route path="otpconfirm" element={<Otpconfirm />} />
               <Route path="logout" element={<Logout />} />
